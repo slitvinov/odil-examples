fb172f5d0d42b9e05d92995b8885fec8a20d277d
Sergey Litvinov <slitvinov@gmail.com>
2025-09-25 11:21:36 -0400

    wave.py

--- wave.py
+++ wave.py
@@ -15,15 +15,15 @@ col = []
 rhs = []
 data = []
 nx = 50
-nt = 10
+nt = 50
 L = 1.0
-T = 10.0
+T = 1.0
 sigma = 0.2
 dx = 2 * L / (nx - 1)
 dt = T / nt
-c0 = -1/(2*dx**2)
-c1 = 1/(2*dt**2)
-c2 = -((dx**2-dt**2)/(dt**2*dx**2))
+c0 = -1 / (2 * dx**2)
+c1 = 1 / (2 * dt**2)
+c2 = -((dx**2 - dt**2) / (dt**2 * dx**2))
 x = np.linspace(-L, L, nx)
 for j in range(nx):
     cappend(0, j, 1)
@@ -37,15 +37,16 @@ for i in range(1, nt - 1):
             cappend(i, j, 1)
             rhs.append(0)
         else:
-            cappend(i-1, j, c1)
-            cappend(i, j-1, c0)
+            cappend(i - 1, j, c1)
+            cappend(i, j - 1, c0)
             cappend(i, j, c2)
-            cappend(i, j+1, c0)
-            cappend(i+1, j, c1)
+            cappend(i, j + 1, c0)
+            cappend(i + 1, j, c1)
             rhs.append(0)
 A = scipy.sparse.csr_matrix((data, (row, col)), dtype=float)
 sol = scipy.sparse.linalg.spsolve(A, rhs)
 u = np.asarray(sol).reshape(nt, nx)
 for k in 0, nt // 4, nt // 2, 3 * nt // 4, nt - 1:
     plt.plot(x, u[k, :], 'o-', label=f"t={k*dt:.2f}")
+plt.legend()
 plt.show()
